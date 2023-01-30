6125a5a98a243b71ce13793010cfe47542192c20
bomb1980 <bombbomb1980@gmail.com>
2023-01-30 14:55:48 +0700

    Update CronempController.php

--- framework/controllers/CronempController.php
+++ framework/controllers/CronempController.php
@@ -12,36 +12,69 @@ use app\models\PerPersonal1;
 class CronempController extends Controller
 {
 
-    // http://samservice/cronemp/
-    public function actionIndex()
-    {
 
-        $request = Yii::$app->request;
-        $uid = $request->get("uid"); //'ksukrit'
+	function getAuthorizationHeader()
+	{
 
-        // $password = $request->get("password","sukrit6290" );
+		$headers = null;
 
-        $MasUser = MasUser::find()
-            ->where(['uid' => $uid])
-            ->one();
+		if (function_exists('apache_request_headers')) {
+			$requestHeaders = apache_request_headers();
+			// Server-side fix for bug in old Android versions (a nice side-effect of this fix means we don't care about capitalization for Authorization)
+			$requestHeaders = array_combine(array_map('ucwords', array_keys($requestHeaders)), array_values($requestHeaders));
+			//print_r($requestHeaders);
+			if (isset($requestHeaders['Authorization'])) {
+				$headers = trim($requestHeaders['Authorization']);
+			}
+		}
+		return $headers;
+	}
 
-        if ($MasUser) {
-
-            // if (password_verify($password, $MasUser->password)) {
 
 
-            // }
-            
-            echo PerPersonal1::getFromApi($MasUser->id);
-            exit;
-        }
+	// http://samservice/cronemp/
+	public function actionIndex()
+	{
 
-        $datas['status'] = 'fail';
-        $datas['msg'] = 'ไม่สำเร็จ';
+		$headers = $this->getAuthorizationHeader();
 
-        echo json_encode( $datas );
+		if (preg_match('/Bearer\s(\S+)/', $headers, $matches)) {
 
-        exit;
 
-    }
+			// arr($matches);
+
+			// $request = Yii::$app->request;
+			$uid = $matches[1];
+			
+			// $request->get("uid"); //'ksukrit'
+
+			// $password = $request->get("password","sukrit6290" );
+
+			$MasUser = MasUser::find()
+				->where(['uid' => $uid])
+				->one();
+
+			if ($MasUser) {
+
+				// if (password_verify($password, $MasUser->password)) {
+
+
+				// }
+
+				echo PerPersonal1::getFromApi($MasUser->id);
+				exit;
+			}
+
+
+
+			//   return $matches[1];
+		}
+
+		$datas['status'] = 'fail';
+		$datas['msg'] = 'ไม่สำเร็จ';
+
+		echo json_encode($datas);
+
+		exit;
+	}
 }
