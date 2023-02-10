ffea3a08f8504267a4a06f0e471153fec2e68a0a
bomb1980 <bombbomb1980@gmail.com>
2023-02-10 15:44:00 +0700

    t

--- a_migrations/public/js/custom.js
+++
@@ -1,5 +0,0 @@
-$('input[type="number"]').on("keydown", function (e) {
-    if (e.which == 38 || e.which == 40 || e.which == 69) {
-        e.preventDefault();
-    }
-});
--- a_migrations/resources/views/layouts/footer.blade.php
+++
@@ -1,7 +0,0 @@
-<footer class="site-footer">
-    <div class="container-fluid">
-        <div class="copyright text-center">
-            สงวนลิขสิทธิ์ © 2564 สำนักงานปลัดกระทรวงแรงงาน
-        </div>
-    </div>
-</footer>
