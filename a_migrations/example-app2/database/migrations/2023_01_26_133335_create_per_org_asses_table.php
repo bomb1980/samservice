14f38eb6a383791af212684e901a9ac3799640b0
bomb1980 <bombbomb1980@gmail.com>
2023-02-14 16:19:00 +0700

    Update 2023_01_26_133335_create_per_org_asses_table.php

--- a_migrations/example-app2/database/migrations/2023_01_26_133335_create_per_org_asses_table.php
+++ a_migrations/example-app2/database/migrations/2023_01_26_133335_create_per_org_asses_table.php
@@ -12,6 +12,21 @@ class CreatePerOrgAssesTable extends Migration
     {
         Schema::create( 'per_org_ass', function (Blueprint $table) {
                 $table->integer('id')->autoIncrement();
+                $table->string("org_dopa_code")->nullable();
+
+                $table->string("org_job")->nullable();
+
+                $table->string("org_addr1")->nullable();
+                $table->string("org_addr2")->nullable();
+                $table->string("org_addr3")->nullable();
+                $table->string("org_seq_no")->nullable();
+
+                $table->string("department_id")->nullable();
+                $table->string("org_id_ass")->nullable();
+
+                $table->string("update_date")->nullable();
+                $table->string("update_user")->nullable();
+
                 $table->integer("bomb")->default(0);
                 $table->integer("org_id")->nullable();
                 $table->integer("org_id_")->nullable();
@@ -22,30 +37,21 @@ class CreatePerOrgAssesTable extends Migration
                 $table->string("org_short")->nullable();
                 $table->string("ol_code")->nullable();
                 $table->string("ot_code")->nullable();
-                $table->string("org_addr1")->nullable();
-                $table->string("org_addr2")->nullable();
-                $table->string("org_addr3")->nullable();
+
                 $table->string("ap_code")->nullable();
                 $table->string("pv_code")->nullable();
                 $table->string("ct_code")->nullable();
                 $table->string("org_date")->nullable();
-                $table->string("org_job")->nullable();
                 $table->string("org_id_ref")->nullable();
                 $table->string("org_active")->nullable();
-                $table->string("update_user")->nullable();
-                $table->string("update_date")->nullable();
                 $table->string("org_website")->nullable();
-                $table->string("org_seq_no")->nullable();
-                $table->string("department_id")->nullable();
                 $table->string("org_eng_name")->nullable();
                 $table->string("pos_lat")->nullable();
                 $table->string("pos_long")->nullable();
-                $table->string("org_dopa_code")->nullable();
                 $table->string("dt_code")->nullable();
                 $table->string("mg_code")->nullable();
                 $table->string("pg_code")->nullable();
                 $table->string("org_zone")->nullable();
-                $table->string("org_id_ass")->nullable();
             }
         );
     }
