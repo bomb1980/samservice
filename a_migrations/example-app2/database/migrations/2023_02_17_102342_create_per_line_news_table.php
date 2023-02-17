9300bb9e704f6031d2a0875e8e7fa9d4a55b0ec4
bomb1980 <bombbomb1980@gmail.com>
2023-02-17 10:26:18 +0700

    Create 2023_02_17_102342_create_per_line_news_table.php

---
+++ a_migrations/example-app2/database/migrations/2023_02_17_102342_create_per_line_news_table.php
@@ -0,0 +1,57 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePerLineNewsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+
+
+        Schema::create('per_line_news', function (Blueprint $table) {
+            $table->string('line_id')->nullable();
+            $table->string('line_code')->nullable();
+            $table->string('linename_abbr')->nullable();
+            $table->string('linename_th')->nullable();
+            $table->string('linename_en')->nullable();
+            $table->string('linegroup_id')->nullable();
+            $table->string('positiontype_id')->nullable();
+            $table->string('pertype_id')->nullable();
+            $table->string('level_min')->nullable();
+            $table->string('level_max')->nullable();
+            $table->string('sortorder')->nullable();
+            $table->string('flag')->nullable();
+            $table->string('std_code')->nullable();
+            $table->string('creator')->nullable();
+            $table->string('createdate')->nullable();
+            $table->string('updateuser')->nullable();
+            $table->string('updatedate')->nullable();
+            $table->string('create_org')->nullable();
+            $table->string('recode_id')->nullable();
+            $table->string('is_sync')->nullable();
+            $table->string('sync_datetime')->nullable();
+            $table->string('sync_status_code')->nullable();
+            $table->string('is_delete')->nullable();
+            $table->string('update_org')->nullable();
+            $table->string('org_owner')->nullable();
+            $table->string('org_visible')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('per_line_news');
+    }
+}
