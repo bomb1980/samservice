4a4d0d64eeb242d9d33f315c529173529f57f48b
bomb1980 <bombbomb1980@gmail.com>
2023-02-17 17:22:26 +0700

    t

--- a_migrations/example-app2/routes/web.php
+++ a_migrations/example-app2/routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
 
+use Illuminate\Support\Facades\Route;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+
+Route::get('/gogo', [App\Http\Controllers\DasdfsdsadfController::class, 'index'])->name('gogo');
